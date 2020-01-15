327f3b2724930bf2bd387dfcfa70f7dcfa4b44e8
Abhishek Verma <absk1317@gmail.com>
2020-01-15 20:12:37 +0530

    Enhance redis expiry by conditionals

--- app/services/trends/base.rb
+++ app/services/trends/base.rb
@@ -14,9 +14,22 @@ module Trends
     end
 
     # expire the cache in 60 s by default
-    def redis_set(key, value, expiry = 60)
+    def redis_set(key, value, expiry = fetch_expiry)
       redis.set key, value.to_json
       redis.expire key, expiry
+    end
+
+    def fetch_expiry
+      case since
+      when 'daily'
+        60 # 60 seconds expiry for daily
+      when 'weekly'
+        600 # 10 minutes expiry for weekly
+      when 'monthly'
+        6000 # 100 minutes expiry for monthly
+      else
+        100 # 100 seconds otherwise
+      end
     end
   end
 end
